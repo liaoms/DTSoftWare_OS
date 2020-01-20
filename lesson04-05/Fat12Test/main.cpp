dbca660392919c1ee42c4eb9a1cd36f4beebcc28
liaoms <763905926@qq.com>
2020-01-20 15:58:37 +0800

    获取根目录文件项

--- lesson04-05/Fat12Test/main.cpp
+++ lesson04-05/Fat12Test/main.cpp
@@ -13,7 +13,7 @@ struct Fat12Header
     uchar BPB_SecPerClus;   //每簇扇区数
     ushort BPB_RsvdSecCnt;
     uchar BPB_NumFATs;
-    ushort BPB_RootEntCnt;  //最大根目录文件数
+    ushort BPB_RootEntCnt;  //最大跟目录文件数
     ushort BPB_TotSec16;
     uchar BPB_Media;
     ushort BPB_FATSz16;
@@ -27,6 +27,17 @@ struct Fat12Header
     uint BS_VolID;
     char BS_VolLab[11];
     char BS_FileSysType[8];
+};
+
+struct RootEntry
+{
+    char DIR_Name[11];
+    uchar DIR_Attr;
+    uchar reserve[10];
+    ushort DIR_WrtTime;
+    ushort DIR_WrtDate;
+    ushort DIR_FstClus;
+    uint DIR_FileSize;
 };
 
 #pragma pack(pop)
@@ -83,14 +94,62 @@ void PrintHeader(Fat12Header& rf, QString p)
     file.close();
 }
 
+//获取一个根目录项
+RootEntry FindRootEntry(Fat12Header& rf, QString p, int i)
+{
+    RootEntry ret = {{0}};
+
+    QFile file(p);
+
+    //BPB_RootEntCnt为最大根目录文件数
+    if( file.open(QIODevice::ReadOnly) && (0 <= i) && (i < rf.BPB_RootEntCnt) )
+    {
+        QDataStream in(&file);
+
+        //定位到19扇区的各个根目录项开始处
+        file.seek(19 * rf.BPB_BytsPerSec + i * sizeof(RootEntry));
+        //每次只读一个根目录项
+        in.readRawData(reinterpret_cast<char*>(&ret), sizeof(ret));
+    }
+
+    file.close();
+
+    return ret; //返回读取到的根目录项
+}
+
+//打印根目录项
+void PrintRootEntry(Fat12Header& rf, QString p)
+{
+    //依次遍历每个根目录项并获取打印
+    for(int i=0; i<rf.BPB_RootEntCnt; i++)
+    {
+        RootEntry re = FindRootEntry(rf, p, i);
+        if( re.DIR_Name[0] != '\0' )
+        {
+            qDebug() << i << ":";
+            qDebug() << "DIR_Name: " << hex << re.DIR_Name;
+            qDebug() << "DIR_Attr: " << hex << re.DIR_Attr;
+            qDebug() << "DIR_WrtDate: " << hex << re.DIR_WrtDate;
+            qDebug() << "DIR_WrtTime: " << hex << re.DIR_WrtTime;
+            qDebug() << "DIR_FstClus: " << hex << re.DIR_FstClus;
+            qDebug() << "DIR_FileSize: " << hex << re.DIR_FileSize;
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    QString strImg = "E:\\DTSoftWare\\Code\\Fat12Test\\data.img";
 
     Fat12Header f12;
 
     //读取并打印data.img主引导程序(第0扇区)的关键信息
-    PrintHeader(f12, "E:\\DTSoftWare\\Code\\Fat12Test\\data.img");
+    PrintHeader(f12, strImg);
+
+    qDebug() << "*****************";
+    //读取根目录项信息
+    PrintRootEntry(f12, strImg);
 
     return a.exec();
 }
