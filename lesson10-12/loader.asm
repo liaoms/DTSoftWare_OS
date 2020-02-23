5ae1e74436cd606f4e85c25117c3eca9844bd17c
liaoms <763905926@qq.com>
2020-02-24 00:44:05 +0800

    定义打印函数打印字符串、优化代码

--- lesson10-12/loader.asm
+++ lesson10-12/loader.asm
@@ -9,8 +9,10 @@ jmp CODE16_SEGMENT	;跳转执行
 ;
 ;                                 段基址       段界限              段属性
 GDT_ENTRY    :     Descriptor        0,          0,                   0        ;全局段入口(0占位)
-CODE32_DESC  :     Descriptor        0,    Code32SegLen -1,       DA_C + DA_32 ;定义第一个32位代码段
+CODE32_DESC  :     Descriptor        0,    Code32SegLen -1,       DA_C + DA_32 ;定义第一个32位代码段描述符
 VIDED_DESC   :     Descriptor     0xB8000,     0x07FFF,           DA_DRWA + DA_32 ;	定义一个显示段范围0xB8000~0xBFFFF，段界限为偏移地址的最大值(0xBFFFF-0xB8000)属性为 已访问的可读写数据段 + 保护模式下32位段
+DATA32_DESC  :     Descriptor        0,    Data32SegLen - 1,      DA_DR + DA_32  ; 定义数据段描述符   
+STACK_DESC   :     Descriptor        0,    TopOfStackInit,        DA_DRW + DA_32 ;定义全局段描述符(栈空间)
 
 ; GDT end
 
@@ -24,8 +26,23 @@ GdtPtr:	;全局段描述符地址
 
 Code32Selector    equ (0x001 << 3) + SA_TIG + SA_RPL0  ;第一个代码段的选择子下标为1 (0x001)
 VideoSelector     equ (0x002 << 3) + SA_TIG + SA_RPL0  ;显示段的选择子下标为2 (0x002)
+Data32Selector    equ (0x003 << 3) + SA_TIG + SA_RPL0  ;数据段的选择子下标为3 (0x003)
+StackSelector     equ (0x004 << 3) + SA_TIG + SA_RPL0  ;全局栈的选择子下标为3 (0x003)
 
 ; end of [section .gdt]
+
+TopOfStackInit equ 0x7c00   ;定义常量，初始栈指针位置
+
+;定义一个32位模式的数据段
+[section .dat]
+[bits 32]
+DATA32_SEGMENT:
+	DTOS    db "D.T.OS!", 0  ;以0结尾的字符串 "D.T.OS!"
+	DTOS_OFFSET equ DTOS - $$ ;字符串"D.T.OS!" 在数据段的偏移地址
+
+Data32SegLen equ $ - DATA32_SEGMENT  ;数据段长度
+
+
 
 ;实模式的代码段定义
 [section .s16]
@@ -35,17 +52,27 @@ CODE16_SEGMENT:
 	mov ds, ax
 	mov es, ax
 	mov ss, ax
-	mov sp, 0x7c00
+	mov sp, TopOfStackInit
 
 	;初始化32位的代码段的段基地址
-	mov eax, 0	;eax寄存器为ax寄存器的延伸，有32位
-	mov ax, cs
-	shl eax, 4
-	add eax, CODE32_SEGMENT
-	mov word [CODE32_DESC + 2], ax	;初始化代码段低32位的31-16位的段基地址(位置为增加2字处)
-	shr eax, 16
-	mov byte [CODE32_DESC + 4], al	;初始化代码段高32位的7-0位的段基地址(位置为增加4字节处)
-	mov byte [CODE32_DESC + 7], ah	;初始化代码段高32位的31-14位的段基地址(位置为增加7字节处)
+	;mov eax, 0	;eax寄存器为ax寄存器的延伸，有32位
+	;mov ax, cs
+	;shl eax, 4
+	;add eax, CODE32_SEGMENT
+	;mov word [CODE32_DESC + 2], ax	;初始化代码段低32位的31-16位的段基地址(位置为增加2字处)
+	;shr eax, 16
+	;mov byte [CODE32_DESC + 4], al	;初始化代码段高32位的7-0位的段基地址(位置为增加4字节处)
+	;mov byte [CODE32_DESC + 7], ah	;初始化代码段高32位的31-14位的段基地址(位置为增加7字节处)
+	
+	;代码段初始化
+	mov esi, CODE32_SEGMENT
+	mov edi, CODE32_DESC	
+	call InitDescItem
+	
+	;数据段初始化
+	mov esi, DATA32_SEGMENT
+	mov edi, DATA32_DESC	
+	call InitDescItem
 	
 	; 初始化GdTPtr结构体值
 	mov eax, 0
@@ -72,6 +99,25 @@ CODE16_SEGMENT:
 	
 	;5-从16位模式跳转到32位模式
 	jmp dword Code32Selector : 0
+
+;定义段描述符初始化函数
+; esi -> 代码段标签
+; edi -> 段描述符标签	
+InitDescItem:
+	
+	push eax
+	
+	mov eax, 0	;eax寄存器为ax寄存器的延伸，有32位
+	mov ax, cs
+	shl eax, 4
+	add eax, esi
+	mov word [edi + 2], ax	;初始化代码段低32位的31-16位的段基地址(位置为增加2字处)
+	shr eax, 16
+	mov byte [edi + 4], al	;初始化代码段高32位的7-0位的段基地址(位置为增加4字节处)
+	mov byte [edi + 7], ah	;初始化代码段高32位的31-14位的段基地址(位置为增加7字节处)
+	
+	pop eax
+	ret
 	
 ;定义32位代码段
 [section .s32]	
@@ -80,12 +126,58 @@ CODE32_SEGMENT:
     mov ax, VideoSelector    ;加载显示段
 	mov gs, ax
 	
-	mov edi, (80*12 + 37)*2  ;显示位置为第12行、37列，(每行显示80个字符，所以是80*12)，一个文本占2字节，所以*2
-	mov ah, 0x0c	;显示方式为黑底红字
-	mov al, 'p'		;打印一个字符p
-	mov [gs:edi], ax ;将ax放到显存段gs，偏移地址为edi的位置处
+	mov ax, StackSelector ;设置栈空间(32位模式下有打印函数调用)
+	mov ss, ax
+	
+	;设置参数并调用打印函数
+	mov ax, Data32Selector ;数据段选择子
+	mov ds, ax
+	mov ebp, DTOS_OFFSET  ;字符在数据段的偏移地址
+	mov bx, 0x0c 	;打印属性黑底红字
+	mov dh, 12 ;打印位置 12行33列
+	mov dl, 33
+	
+	call PrintString
 	
     jmp $
+	
+	
+;定义32位模式下的打印函数
+;ds:ebp -> 字符串地址
+;bx -> 打印属性
+;dx -> 打印位置 dh(行), dl(列)
+PrintString:	
+	push ebp
+	push eax
+	push edi
+	push dx
+	push cx
+		
+print:
+	mov cl, [ds:ebp] ;一个个加载字符串内容
+	cmp cl, 0
+	je end
+	mov eax, 80  
+	mul dh  ;计算位置(行)
+	add al, dl ;打印位置(列)
+	shl eax, 1 ; * 2
+	mov edi, eax
+	mov ah, bl ;打印属性
+	mov al, cl ;打印内容
+	mov [gs:edi], ax ;放到显存中
+	inc ebp	;下一个打印字符
+	inc dl  ;下一个打印位置
+	
+	jmp print
+		
+end:
+	pop cx
+	pop dx
+	pop edi
+	pop eax
+	pop ebp
+	
+	ret
 
 Code32SegLen    equ $ - CODE32_SEGMENT    ;定义32位代码段段界限
 
